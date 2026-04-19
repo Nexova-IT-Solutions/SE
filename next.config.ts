4c796907978fc76d96c7c8fa78d1d9d85164eb74
praneeth-nexova <praneeth@nexovaitsolutions.com>
2026-04-19 17:25:15 +0530

    netlify issue fix

--- next.config.ts
+++ next.config.ts
@@ -5,7 +5,7 @@ import createNextIntlPlugin from 'next-intl/plugin';
 const withNextIntl = createNextIntlPlugin();
 
 const nextConfig: NextConfig = {
-  output: "standalone",
+  // output: "standalone", // Disabled for Netlify compatibility
   /* config options here */
   typescript: {
     ignoreBuildErrors: true,
